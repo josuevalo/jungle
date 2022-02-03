13cc2c8d63510955efb9c9e567885a60c035407f
Josue <arevalo.josued@gmail.com>
2022-02-03 21:33:51 +0000

    Created User model, route, users controller with actions

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,11 @@ Rails.application.routes.draw do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
     resources :categories, except: [:edit, :update, :show]
+
+    
+    get '/signup' => 'users#new'
+    post '/users' => 'users#create'
+
   end
 
   # The priority is based upon order of creation: first created -> highest priority.
