595466aaf3eddd8804246fc43e2c63621fb5e9c6
Josue <arevalo.josued@gmail.com>
2022-02-03 19:44:31 +0000

    Adjusted all admin categories files to do the index (list), new and create actions for categories

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,2 +1,36 @@
 class Admin::CategoriesController < ApplicationController
+
+
+  http_basic_authenticate_with :name => ENV['HTTP_NAME'], :password => ENV['HTTP_PASSWORD'] 
+  
+  def index
+    @categories = Category.all
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      redirect_to [:admin, :categories], notice: 'Category created!'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(
+      :name,
+      :description,
+      :category_id,
+      :quantity,
+
+    )
+  end
+
 end
