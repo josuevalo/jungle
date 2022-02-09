0a3c67377a41f82e5450bfea5a9ed4600da8ad12
Josue <arevalo.josued@gmail.com>
2022-02-09 23:46:39 +0000

    fix :name during login and create new user. Cleanup of dead code in other files

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,14 +8,15 @@ class UsersController < ApplicationController
       session[:user_id] = user.id
       redirect_to '/'
     else
-      redirect_to '/signup'
+     
+      redirect_to '/signup', notice: "Oops! Something went wrong. Please try again"
     end
   end
 
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
   end
 
 end
